535220aeb98ebaa3fbb46f5c3d1c98df65cd9eb1
Madhura <madhuraweerasooriye@gmail.com>
2023-01-06 08:05:02 +0530

    Initial Commit

---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Madhura on 2023-01-06.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
